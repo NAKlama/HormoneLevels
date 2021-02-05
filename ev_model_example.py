73773b10fd9e7cdac7e1725e0a133ef403b155de
Nina Alexandra Klama <nina-alexandra@klama.name>
2021-02-05 13:42:59 +0100

    Improved example to show how to use lab data.

--- ev_model_example.py
+++ ev_model_example.py
@@ -34,44 +34,63 @@ model.add_dose(ev, 2.5, datetime(2020, 12, 18, 11))
 model.add_dose(ev, 2.5, datetime(2020, 12, 21, 9))
 model.add_dose(ev, 2.5, datetime(2020, 12, 23, 9))
 model.add_dose(ev, 2.5, datetime(2020, 12, 26, 15))
+model.add_dose(ev, 2.5, datetime(2020, 12, 29, 11))
+model.add_dose(ev, 2.5, datetime(2021,  1,  1, 12))
+model.add_dose(ev, 2.5, datetime(2021,  1,  4, 9))
+model.add_dose(ev, 2.5, datetime(2021,  1,  7, 12))
+model.add_dose(ev, 2.5, datetime(2021,  1,  9, 9))
+model.add_dose(ev, 2.5, datetime(2021,  1, 12, 9))
+model.add_dose(ev, 2.5, datetime(2021,  1, 15, 9))
+model.add_dose(ev, 3.0, datetime(2021,  1, 18, 9))
+model.add_dose(ev, 2.4, datetime(2021,  1, 21, 9))
+model.add_dose(ev, 2.9, datetime(2021,  1, 24, 9))
+model.add_dose(ev, 2.7, datetime(2021,  1, 27, 10))
+model.add_dose(ev, 3.0, datetime(2021,  1, 30, 10))
+model.add_dose(ev, 2.5, datetime(2021,  2, 1, 9))
+model.add_dose(ev, 2.9, datetime(2021,  2, 3, 8))
+
+model.add_lab_data([
+    LabData(datetime(2021, 1, 7, 12), {ev: 214.0}),
+    LabData(datetime(2021, 1, 25, 12), {ev: 251.0})
+])
 
 days_into_future = 30
 model.calculate_timeline(date.today() + timedelta(days=days_into_future))
+model.estimate_blood_levels()
 now = float((datetime.today() - datetime.combine(model.starting_date, time())).total_seconds()) / (3600 * 24)
 
 fortnight_ago = model.duration - (14+days_into_future)*24
 month_ago = model.duration - (30+days_into_future)*24
 
-y_window = (0, 8)
+# y_window = (0, 8)
+y_window = (0, 400)
 
-plot_drugs(data=model.get_plot_data(),
+# confidence = 68
+# data = model.get_plot_data(True)
+
+confidence = 95
+data = model.get_plot_data(True, sd_mult=2)
+
+lab_levels = model.get_plot_lab_levels()
+
+plot_drugs(data=data,
            x_window=(0, model.duration/24.0),
            y_window=y_window,
+           y_label="Estimated blood levels (ng/l)",
+           lab_data=lab_levels,
+           confidence_val=confidence,
            now=now)
-plot_drugs(data=model.get_plot_data(),
+plot_drugs(data=data,
            x_window=(fortnight_ago/24.0, (model.duration/24.0)-16),
            y_window=y_window,
+           y_label="Estimated blood levels (ng/l)",
+           lab_data=lab_levels,
+           confidence_val=confidence,
            now=now)
-plot_drugs(data=model.get_plot_data(),
+plot_drugs(data=data,
            x_window=(month_ago/24.0, (model.duration/24.0)-16),
            y_window=y_window,
+           y_label="Estimated blood levels (ng/l)",
+           lab_data=lab_levels,
+           confidence_val=confidence,
            now=now)
-
-
-# # 12.12. 9:00
-# d, t = add_dose(d, 2.5, t, int(24), output)  # 13.12. 9:00
-# d, t = add_dose(d, 2.5, t, int(2 * 24 + 2), output)  # 15.12. 9:00
-# d, t = add_dose(d, 2.5, t, int(3 * 24 - 2), output)  # 18.12. 11:00
-# d, t = add_dose(d, 2.5, t, int(3 * 24), output)  # 21.12. 9:00
-# d, t = add_dose(d, 2.5, t, int(2 * 24 + 6), output)  # 23.12. 9:00
-#
-# d, t = add_dose(d, 2.5, t, int(3 * 24 - 4), output)  # 26.12. 15:00
-# d, t = add_dose(d, 2.5, t, int(3 * 24 + 1), output)  # 29.12. 11:00
-# d, t = add_dose(d, 2.5, t, int(3 * 24), output)  # 01.01. 12:00
-# d, t = add_dose(d, 2.5, t, int(3 * 24 - 3), output)  # 04.01. 9:00
-# d, t = add_dose(d, 2.5, t, int(3 * 24 + 3), output)  # 07.01. 12:00
-# d, t = add_dose(d, 2.5, t, int(2 * 24 - 3), output)  # 09.01. 9:00
-# d, t = add_dose(d, 2.5, t, int(3 * 24), output)  # 12.01. 9:00
-# d, t = add_dose(d, 2.5, t, int(3 * 24), output)  # 15.01. 9:00
-# d, t = add_dose(d, 3.0, t, int(3 * 24), output)  # 18.01. 9:00
-# d, t = add_dose(d, 2.4, t, int(3 * 24), output)  # 20.01. 9:00
