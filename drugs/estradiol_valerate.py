044b38a8b8ab03513337668585b9847bff57819c
Nina Alexandra Klama <nina-alexandra@klama.name>
2021-05-17 18:24:41 +0200

    Mild improvement of Model.

--- drugs/estradiol_valerate.py
+++ drugs/estradiol_valerate.py
@@ -23,6 +23,6 @@ from datetime import timedelta
 class EstradiolValerate(drug.Drug):
     def __init__(self):
         super().__init__("Estradiol valerate", timedelta(days=4, hours=12))
-        self.set_flood_in([1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        self.set_flood_in([1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
                            7, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, .5, .5])
         self.name_blood = "Estradiol"
