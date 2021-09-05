e7b4a0a3b0c412130835156158873b9a04d22404
Nina Alexandra Klama <nina-alexandra@klama.name>
2021-09-05 12:10:13 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 setup(
     name="HormoneLevels",
     description="Compute and graph hormone levels for transgender HRT",
-    version='0.3',
+    version='0.4.0',
     license="GPLv3",
     author="Nina Alexandra Klama",
     author_email="gitlab@fklama.de",
